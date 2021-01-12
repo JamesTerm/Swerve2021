4c6d195888d54a3999a00810230618b6964953df
JamesTerm <james_killian@hotmail.com>
2021-01-12 15:20:33 -0600

    Added TeleOp V3

--- src/main/cpp/RobotAssembly.cpp
+++ src/main/cpp/RobotAssembly.cpp
@@ -139,8 +139,8 @@ private:
 		//Entity variables-------------------------------------------
 		SmartDashboard::PutNumber("Velocity", Meters2Feet(magnitude));
 		Entity2D::Vector2D position = entity.GetCurrentPosition();
-		SmartDashboard::PutNumber("X", Meters2Feet(position.x));
-		SmartDashboard::PutNumber("Y", Meters2Feet(position.y));
+		SmartDashboard::PutNumber("X_ft", Meters2Feet(position.x));
+		SmartDashboard::PutNumber("Y_ft", Meters2Feet(position.y));
 		//for swerve the direction of travel is not necessarily the heading, so we show this as well as heading
 		SmartDashboard::PutNumber("Travel_Heading", RAD_2_DEG(atan2(velocity_normalized[0], velocity_normalized[1])));
 		SmartDashboard::PutNumber("Heading", RAD_2_DEG(entity.GetCurrentHeading()));
@@ -317,8 +317,8 @@ private:
 		//Entity variables-------------------------------------------
 		SmartDashboard::PutNumber("Velocity", Meters2Feet(magnitude));
 		Entity2D::Vector2D position = entity.GetCurrentPosition();
-		SmartDashboard::PutNumber("X", Meters2Feet(position.x));
-		SmartDashboard::PutNumber("Y", Meters2Feet(position.y));
+		SmartDashboard::PutNumber("X_ft", Meters2Feet(position.x));
+		SmartDashboard::PutNumber("Y_ft", Meters2Feet(position.y));
 		//for swerve the direction of travel is not necessarily the heading, so we show this as well as heading
 		SmartDashboard::PutNumber("Travel_Heading", RAD_2_DEG(atan2(velocity_normalized[0], velocity_normalized[1])));
 		//This is temporary and handy for now, will change once we get AI started
@@ -479,15 +479,222 @@ public:
 	}
 };
 #pragma endregion
+#pragma region _05 Test_Swerve_Viewer Tele V3_
+//This is the last of the stand-alone tele versions, which introduces the super assembly of swerve robot
+//so a lot of the hooking is done inside of it, keeping the minimal hooking here... so when switching over
+//from simulation to actual motors can be resolved from within that assembly
+//This assembly and onward start to show the use-case of writing hooks through lambda operators, ask me if you need
+//help reading this part of the code.
+
+#include "Modules/Robot/SwerveRobot/SwerveRobot.h"
+
+class Test_Swerve_Rotary
+{
+private:
+	#pragma region _member variables_
+	Module::Localization::Entity2D m_Entity;
+	//Here we can choose which motion control to use, this works because the interface
+	//between them remain (mostly) identical
+	Module::Input::wpi_Joystick m_joystick;  //Note: always late binding, so we can aggregate direct easy here
+	Module::Input::Analog_EventEntry m_joystick_options;  //for now a simple one-stop option for all
+	Module::Robot::SwerveRobot m_robot;  //keep track of our intended velocities
+
+	//These typically are constants, but if we have gear shifting they can change
+	double m_maxspeed; //max velocity forward in feet per second
+	double m_max_heading_rad;  //max angular velocity in radians
+	double m_dTime_s=0.016;  //cached so other callbacks can access
+	#pragma endregion
+
+	void UpdateVariables()
+	{
+        using namespace frc;
+		Module::Localization::Entity2D &entity = m_Entity;
+		using namespace Module::Localization;
+		const Entity2D::Vector2D linear_velocity = entity.GetCurrentVelocity();
+		Vec2D velocity_normalized(linear_velocity.x, linear_velocity.y);
+		double magnitude = velocity_normalized.normalize();
+		//Entity variables-------------------------------------------
+		SmartDashboard::PutNumber("Velocity", Meters2Feet(magnitude));
+		SmartDashboard::PutNumber("Rotation Velocity", m_Entity.GetCurrentAngularVelocity());
+		Entity2D::Vector2D position = entity.GetCurrentPosition();
+		SmartDashboard::PutNumber("X_ft", Meters2Feet(position.x));
+		SmartDashboard::PutNumber("Y_ft", Meters2Feet(position.y));
+		//This is temporary and handy for now, will change once we get AI started
+		//Like with the kinematics if we are not moving we do not update the intended orientation (using this)
+		//This is just cosmetic, but may be handy to keep for teleop
+		if (!IsZero(linear_velocity.x + linear_velocity.y, 0.02))
+        {
+			//m_current_state.bits.IntendedOrientation = atan2(velocity_normalized[0], velocity_normalized[1]);
+       		//for swerve the direction of travel is not necessarily the heading, so we show this as well as heading
+    		SmartDashboard::PutNumber("Travel_Heading", RAD_2_DEG(atan2(velocity_normalized[0], velocity_normalized[1])));
+        }
+		else if (!IsZero(entity.GetCurrentAngularVelocity()))
+		{
+			//point forward locally when rotating in place
+			//m_current_state.bits.IntendedOrientation = entity.GetCurrentHeading(); 
+            SmartDashboard::PutNumber("Travel_Heading", RAD_2_DEG(entity.GetCurrentHeading()));
+		}
+		SmartDashboard::PutNumber("Heading", RAD_2_DEG(entity.GetCurrentHeading()));
+		//To make this interesting, we keep the SmartDashboard to show the intended velocities...
+		//SmartDashboard::PutNumber("setpoint_angle", RAD_2_DEG(entity.Get_IntendedOrientation()));
+		//kinematic variables-------------------------------------------
+		const Module::Robot::SwerveVelocities &cv = m_robot.GetCurrentVelocities();
+		const Module::Robot::SwerveVelocities &iv = m_robot.GetIntendedVelocities();
+		const Module::Robot::SwerveVelocities &vo = m_robot.GetCurrentVoltages();
+
+		SmartDashboard::PutNumber("Wheel_fl_Velocity", Meters2Feet(iv.Velocity.AsArray[0]));
+		SmartDashboard::PutNumber("Wheel_fr_Velocity", Meters2Feet(iv.Velocity.AsArray[1]));
+		SmartDashboard::PutNumber("Wheel_rl_Velocity", Meters2Feet(iv.Velocity.AsArray[2]));
+		SmartDashboard::PutNumber("Wheel_rr_Velocity", Meters2Feet(iv.Velocity.AsArray[3]));
+		SmartDashboard::PutNumber("Wheel_fl_Voltage", vo.Velocity.AsArray[0]);
+		SmartDashboard::PutNumber("Wheel_fr_Voltage", vo.Velocity.AsArray[1]);
+		SmartDashboard::PutNumber("Wheel_rl_Voltage", vo.Velocity.AsArray[2]);
+		SmartDashboard::PutNumber("Wheel_rr_Voltage", vo.Velocity.AsArray[3]);
+		SmartDashboard::PutNumber("wheel_fl_Encoder", Meters2Feet(cv.Velocity.AsArray[0]));
+		SmartDashboard::PutNumber("wheel_fr_Encoder", Meters2Feet(cv.Velocity.AsArray[1]));
+		SmartDashboard::PutNumber("wheel_rl_Encoder", Meters2Feet(cv.Velocity.AsArray[2]));
+		SmartDashboard::PutNumber("wheel_rr_Encoder", Meters2Feet(cv.Velocity.AsArray[3]));
+
+		//For the angles either show raw or use simple dial using 180 to -180 with a 45 tick interval
+		//its not perfect, but it gives a good enough direction to tell (especially when going down)
+		SmartDashboard::PutNumber("Swivel_fl_Voltage", vo.Velocity.AsArray[4]);
+		SmartDashboard::PutNumber("Swivel_fr_Voltage", vo.Velocity.AsArray[5]);
+		SmartDashboard::PutNumber("Swivel_rl_Voltage", vo.Velocity.AsArray[6]);
+		SmartDashboard::PutNumber("Swivel_rr_Voltage", vo.Velocity.AsArray[7]);
+		SmartDashboard::PutNumber("swivel_fl_Raw", RAD_2_DEG(cv.Velocity.AsArray[4]));
+		SmartDashboard::PutNumber("swivel_fr_Raw", RAD_2_DEG(cv.Velocity.AsArray[5]));
+		SmartDashboard::PutNumber("swivel_rl_Raw", RAD_2_DEG(cv.Velocity.AsArray[6]));
+		SmartDashboard::PutNumber("swivel_rr_Raw", RAD_2_DEG(cv.Velocity.AsArray[7]));
+	}
+	void GetInputSlice()
+	{
+		using namespace Module::Input;
+		//using JoystickInfo = wpi_Joystick::JoystickInfo;
+		size_t JoyNum = 0;
+
+        wpi_Joystick::JoyState joyinfo;
+		//wpi_Joystick::JoyState joyinfo = {0}; //setup joy zero'd out
+        memset(&joyinfo, 0, sizeof(wpi_Joystick::JoyState));
+
+		size_t NoJoySticks = m_joystick.GetNoJoysticksFound();
+		if (NoJoySticks)
+		{
+			//printf("Button: 2=exit, x axis=strafe, y axis=forward/reverse, z axis turn \n");
+			m_joystick.read_joystick(JoyNum, joyinfo);
+		}
+		{
+			//Get an input from the controllers to feed in... we'll hard code the x and y axis from both joy and keyboard
+			//we simply combine them so they can work inter-changeably (e.g. keyboard for strafing, joy for turning)
+			m_robot.SetAngularVelocity((AnalogConversion(joyinfo.Axis.Named.lZ, m_joystick_options) ) * m_max_heading_rad);
+			m_robot.SetLinearVelocity_local(
+				Feet2Meters(m_maxspeed*(AnalogConversion(joyinfo.Axis.Named.lY, m_joystick_options) )*-1.0),
+				Feet2Meters(m_maxspeed*(AnalogConversion(joyinfo.Axis.Named.lX, m_joystick_options) )));
+
+			//This comes in handy for testing
+			if (joyinfo.ButtonBank[0] == 1)
+				Reset();
+		}
+	}
+
+public:
+	void TimeSlice(double dTime_s)
+	{
+		m_dTime_s = dTime_s;
+		//Grab kinematic velocities from controller
+		GetInputSlice();
+		//Update the predicted motion for this time slice
+		m_robot.TimeSlice(dTime_s);
+		m_Entity.TimeSlice(dTime_s);
+		UpdateVariables();
+	}
+
+	void SetUpHooks(bool enable)
+	{
+		if (enable)
+		{
+			//Now to link up the callbacks for the robot motion control:  Note we can link them up even if we are not using it
+			m_robot.Set_UpdateGlobalVelocity([&](const Vec2D &new_velocity)
+			{	m_Entity.SetLinearVelocity_global(new_velocity.y(), new_velocity.x());
+			});
+			m_robot.Set_UpdateHeadingVelocity([&](double new_velocity)
+			{	m_Entity.SetAngularVelocity(new_velocity);
+			});
+			m_robot.Set_GetCurrentPosition([&]() -> Vec2D
+			{
+				//This is a bit annoying, but for the sake of keeping modules independent (not dependent on vector objects)
+				//its worth the hassle
+				Vec2D ret = Vec2D(m_Entity.GetCurrentPosition().x, m_Entity.GetCurrentPosition().y);
+				return ret;
+			});
+			m_robot.Set_GetCurrentHeading([&]() -> double
+			{
+				return m_Entity.GetCurrentHeading();
+			});
+		}
+		else
+		{
+			m_robot.Set_UpdateGlobalVelocity(nullptr);
+			m_robot.Set_UpdateHeadingVelocity(nullptr);
+			m_robot.Set_GetCurrentPosition(nullptr);
+			m_robot.Set_GetCurrentHeading(nullptr);
+		}
+	}
+public:
+	Test_Swerve_Rotary()
+	{
+		SetUpHooks(true);
+	}
+	void Reset()
+	{
+		m_Entity.Reset();
+		m_robot.Reset();
+	}
+
+	void Init()
+	{
+		m_joystick.Init();
+		m_joystick_options = { 
+			0.3,   //filter dead zone
+			1.0,   //additional scale
+			1.0,   // curve intensity
+			false  //is flipped
+			};
+		using namespace Module::Robot;
+		//We'll make a square robot for ease to interpret angles
+		Vec2D wheel_dimensions(Inches2Meters(24), Inches2Meters(24));
+		//Assume our robot's top speed is 12 fps
+		m_maxspeed = 12.0;
+		//I could omit, but I want to show no skid; however, we can reserve this if in practice the 2-3 degrees
+		//of precision loss actually does (I don't believe this would ever be significant)
+		const double skid = 1.0;
+		//We'll compute the max angular velocity to use in equation
+		//like with tank spinning in place needs to be half of spinning with full forward
+		//this time... no skid
+		m_max_heading_rad = (2 * Feet2Meters(m_maxspeed) / wheel_dimensions.length()) * skid;
+
+		//Initialize these before calling reset (as the properties can dictate what to reset to)
+		//TODO provide properties method here and invoke it, init can have properties sent as a parameter here
+		m_robot.Init();
+
+		Reset();  //for entity variables
+	}
+   	~Test_Swerve_Rotary()
+	{
+		m_robot.Shutdown(); //detach hooks early
+		SetUpHooks(false);
+	}
+};
+#pragma endregion
 
 //We just pick what we test or use from here m_teleop declaration
 class TeleOp_Internal
 {
 private:
-//TestJoystick m_teleop;
-//Test03_Swerve_Kinematics_with_Joystick m_teleop;
-//Test_Swerve_Entity_Joystick m_teleop;
-Test_Swerve_Viewer m_teleop;
+//TestJoystick m_teleop;  //01 joystick
+//Test03_Swerve_Kinematics_with_Joystick m_teleop; //02 kinematics and joystick
+//Test_Swerve_Entity_Joystick m_teleop;  //03 simple motion profiling  TeleV1
+//Test_Swerve_Viewer m_teleop;  //04 full profiling TeleV2
+Test_Swerve_Rotary m_teleop;  //05 full tele with rotary TeleV3
 double m_LastTime=0.0;
 
 __inline double GetTime() 
